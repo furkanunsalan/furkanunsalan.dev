3568315970061ac8ab9cf7736be8ee73664acf06
Furkan Ünsalan <me@furkanunsalan.dev>
2024-08-20 15:32:03 +0300

    meta fix

--- app/(pages)/photos/page.tsx
+++ app/(pages)/photos/page.tsx
@@ -16,7 +16,7 @@ export default async function PhotosPage() {
 
   return (
     <>
-      <div className="w-5/6 md:w-1/3 text-center font-mono mx-auto mt-8">This is a collection of moments and scenes I've captured through my lens. Each photo reflects my journey, experiences, and the beauty I see in the world around me. I hope you enjoy exploring my work as much as I enjoyed taking these photos.</div>
+      <div className="w-5/6 md:w-1/3 text-center font-mono mx-auto mt-8">This is a collection of moments and scenes I&apos;ve captured through my lens. Each photo reflects my journey, experiences, and the beauty I see in the world around me. I hope you enjoy exploring my work as much as I enjoyed taking these photos.</div>
       <div className="max-w-2xl mx-auto mt-14 sm:mt-20 px-4">
         {/* Statistics Section */}
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-2 gap-6 mb-12 w-2/3 md:w-full mx-auto">
