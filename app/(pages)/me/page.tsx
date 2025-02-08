ad885155ddc2b0154a07d6f387f5a6d490270396
Furkan Ünsalan <me@furkanunsalan.dev>
2025-02-08 23:16:50 +0300

    fix: ui

--- app/(pages)/me/page.tsx
+++ app/(pages)/me/page.tsx
@@ -160,7 +160,7 @@ export default function Me() {
           ) : books.length === 0 ? (
             <p className="text-center">No books found.</p>
           ) : (
-            <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+            <div className="grid grid-cols-2 sm:grid-cols-2 md:grid-cols-3 gap-4">
               {books.map((book) => (
                 <div
                   key={book.id}
